935ba37e57d68b1b54982fa1c563d98b22d14ee4
bargabay <bar.gabay.home@gmail.com>
2022-08-11 15:16:37 +0300

    close to end

--- textual_ui.js
+++ textual_ui.js
@@ -3,87 +3,75 @@ const { promises } = require("fs");
 const prompt = require('prompt');
 
 const i = 1;
-let functionIndex;
 
-while(i)
-{
-// 3, 8 - 2
+handleUI();
+
+function handleUI() {
+    
+let functionName;
+let functionIndex;
+//while(i)
     handleTextualUIStartMenu();
     prompt.start();
-    prompt.get(['functionIndex'], (err, result) => {
+    prompt.get(['Index'], (err, mainResult) => {
 
         if(err) {
             console.error(err); 
         }
         
         else {
-            functionIndex = result;
+            functionIndex = Number(mainResult.Index); // this performed first, and than going to the ifs
+            console.log(functionIndex);
+
+            if(functionIndex == 3 || functionIndex == 8)
+            {
+                functionName = actByInput(functionIndex);
+                prompt.get(['functionObject1', 'functionObject2'], (err, seconderyResult) => {
+                    if(err) {
+                        console.error(err); 
+                    }
+                    
+                    else { 
+                        eval(functionName)(seconderyResult.functionObject1.toString(), seconderyResult.functionObject2.toString());
+                    }
+                });
+            }
+    
+            if(functionIndex in [1, 2, 4, 5, 6, 7])
+            {
+                functionName = actByInput(functionIndex);
+                prompt.get(['functionObject'], (err, seconderyResult) => {
+                    if(err) {
+                        console.error(err); 
+                    }
+                    
+                    else { 
+                        console.log(seconderyResult.functionObject.toString());
+                        eval(functionName)(seconderyResult.functionObject.toString());
+                    }
+                });
+            }
+    
+            if(functionIndex == 9)
+                process.exit();
         }
     });
-
-    if(functionIndex == 3 || functionIndex == 8)
-    {
-        prompt.start();
-        prompt.get(['functionObject1', 'functionObject2'], (err, result) => {
-            if(err) {
-                console.error(err); 
-            }
-            
-            else { // change the else actions 
-                functionIndex = result;
-            }
-        });
-        // actByInput(functionIndex);
-    }
-
-    if(functionIndex in [2, 4, 5, 6, 7, 9])
-    {
-        prompt.start();
-        prompt.get(['functionObject'], (err, result) => {
-            if(err) {
-                console.error(err); 
-            }
-            
-            else { // change the else actions 
-                functionIndex = result;
-            }
-        });
-        //actByInput(functionIndex);
-    }
-
-    else
-        console.log("wrong index!!!");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function handleTextualUIStartMenu() {
     console.log("to delete file by name type 1.\n\
-                to create file by name type 2.\n\
-                to write text to a file type 3.\n\
-                to create folder by name type 4.\n\
-                to delete folder by name type 5.\n\
-                to create a file in a dictionary type 6.\n\
-                to delete a file from a dictionary type 7.\n\
-                to merge files type 8\n\
-                to exit the system type 9\n");
+to create file by name type 2.\n\
+to write text to a file type 3.\n\
+to create folder by name type 4.\n\
+to delete folder by name type 5.\n\
+to create a file in a dictionary type 6.\n\
+to delete a file from a dictionary type 7.\n\
+to merge files type 8.\n\
+to exit the system type 9.\n");
 }
 
 function deleteFileByName(file) {
-    fs.unlink(file, (err) => {
+    fs.unlinkSync(file, (err) => {
         if (err) {
             console.log(err);
         }
@@ -91,7 +79,7 @@ function deleteFileByName(file) {
 }
 
 function createFileByName(file) {
-    fs.appendFile(file, (err) => {
+    fs.appendFileSync(file, "", (err) => {
         if (err)
             console.log(err);
         else {
@@ -101,7 +89,7 @@ function createFileByName(file) {
 }
 
 function writeToFile(file, text) {
-    fs.appendFile(files, text, (err) => {
+    fs.appendFileSync(files, text, (err) => {
         if (err)
             console.log(err);
         else {
@@ -111,14 +99,9 @@ function writeToFile(file, text) {
 }
 
 function deleteFolderByName(folder) {
-    fs.rmdirSync(folder, (err) => {
-        if (err)
-            console.log(err);
-        else {
-          console.log(`folder ${folder} was deleted.`);
-        }
-	});
-}
+    fs.rmdirSync(folder);
+    console.log(`folder ${folder} was deleted.`);
+}    
 
 function createFolderByName(folder) {
     fs.mkdirSync(folder, (err) => {
@@ -131,7 +114,7 @@ function createFolderByName(folder) {
 }
 
 function deleteFileByNameFromFolder(filePath) {
-    fs.unlink(filePath, (err) => {
+    fs.unlinkSync(filePath, (err) => {
         if (err) {
             console.log(err);
         }
@@ -142,7 +125,7 @@ function deleteFileByNameFromFolder(filePath) {
 }
 
 function createFileByNameFromFolder(filePath) {
-    fs.appendFile(filePath, (err) => {
+    fs.appendFileSync(filePath, "", (err) => {
         if (err)
             console.log(err);
         else {
@@ -152,14 +135,14 @@ function createFileByNameFromFolder(filePath) {
 }
 
 async function mergeFiles(file1, file2) {
-    await promises.readFile(file2, (err, content) => { // read
+    await promises.readFileSync(file2, (err, content) => { // read
         if (err)
             console.log(err);
         else {
             console.log("file was read.");
         }
 
-        fs.appendFile(file, content, (err) => {
+        fs.appendFileSync(file, content, (err) => {
             if (err)
                 console.log(err);
             else {
@@ -183,7 +166,7 @@ function actByInput(functionIndex) {
     }
     if(functionIndex == 3) {
         console.log("enter the file name, press enter, and than enter \
-                     the content to write");
+the content to write");
         return "writeToFile";
     }
     if(functionIndex == 4) {
@@ -204,9 +187,7 @@ function actByInput(functionIndex) {
     }
     if(functionIndex == 8) {
         console.log("enter the file name to merge to, press enter, \
-                     and than enter the file name to merge");
+and than enter the file name to merge");
         return "mergeFiles";
     }
-    if(functionIndex == 9)
-        process.exit();
 }
